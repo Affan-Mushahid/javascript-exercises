9380dcd3dc170cdf59dda77e580991ddcd75f2a5
Affan Mushahid <151357646+Affan-Mushahid@users.noreply.github.com>
2025-12-28 21:48:22 +0500

    feat(foundations-09): sum all elements

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    if((Number.isInteger(numA) != true || Number.isInteger(numB) != true)) {
+        return "ERROR";
+    }
+    if((numA < 0 || numB < 0)) {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+
+    for(let i = Math.min(numA, numB); i <= Math.max(numA, numB); i++) sum += i;
+
+    return sum;
 };
 
 // Do not edit below this line
